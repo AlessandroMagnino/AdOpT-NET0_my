360a8590a9db2366843e0fb88de40679d20222e6
Alessandro Magnino <alessandro.magnino@polito.it>
2025-12-09 15:42:38 +0100

    main

--- main.py
+++ main.py
@@ -1,32 +1,72 @@
 # from adopt_net0.model_configuration import ModelConfiguration
 from pathlib import Path
 import adopt_net0.data_preprocessing as dp
+import adopt_net0.database as db
+import adopt_net0.data_preprocessing.model_definition as model
 from adopt_net0.modelhub import ModelHub
 from adopt_net0.result_management.read_results import add_values_to_summary
+import time
 
 # Specify the path to your input data
-path = "specify path to input data"
+input_path = "input"
+output_path = "output"
 
 # Create template files (comment these lines if already defined)
-dp.create_optimization_templates(path)
+dp.create_optimization_templates(input_path, output_path)
+
+# Topology definition
+model.topology_definition(input_path)
 
 # Create folder structure (comment these lines if already defined)
-dp.create_input_data_folder_template(path)
+dp.create_input_data_folder_template(input_path)
+
+# Define nodes locations (comment these lines if already defined)
+model.nodes_location_definition(input_path)
+
+# Define networks (comment these lines if already defined)
+model.networks_definition(input_path)
+
+# Define technologies on each node (comment these lines if already defined)
+model.technologies_definition(input_path)
 
 # Copy technology and network data into folder (comment these lines if already defined)
-dp.copy_technology_data(path, "path to tec data")
-dp.copy_network_data(path, "path to network data")
-dp.copy_compressor_data(path, "path to compressor data")
+dp.copy_technology_data(input_path)
+dp.copy_network_data(input_path)
+dp.copy_compressor_data(input_path)
+
+# Correct data on technologies and networks if needed (comment these lines if already defined)
+model.technologies_data_correction(input_path)
+
+# Correct data on networks if needed (comment these lines if already defined)
+model.networks_data_correction(input_path)
 
 # Read climate data and fill carried data (comment these lines if already defined)
-dp.load_climate_data_from_api(path)
-dp.fill_carrier_data(path, value=0)
-# dp.fill_carrier_pressure_data(path, value=0)
+dp.load_climate_data_from_api(input_path)
+dp.fill_carrier_data(input_path, value_or_data=0)
+dp.fill_carrier_pressure_data(input_path, pressure_value_bar=0)
+
+# Impose carriers data (comment these lines if already defined)
+model.carrier_data_definition(input_path)
+
+# Solver options definition
+model.solver_options_definition(input_path)
+# Optimization options definition
+model.optimization_options_definition(input_path)
 
 # Construct and solve the model
 pyhub = ModelHub()
-pyhub.read_data(path)
+pyhub.read_data(input_path)
+
+# Check time to solve
+
+start_time = time.time()
+
 pyhub.quick_solve()
 
-# Add values of (part of) the parameters and variables to the summary file
-add_values_to_summary(Path("path to summary file"))
+end_time = time.time()
+print(f"\n\nTime to solve: {end_time - start_time} seconds")
+
+# # Add values of (part of) the parameters and variables to the summary file
+# add_values_to_summary(Path("output/Summary.xlsx"))
+
+
