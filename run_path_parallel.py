8068f9790382ce6de3463ba5eb10aef4c82892b3
Alessandro Magnino <alessandro.magnino@polito.it>
2026-06-03 14:47:44 +0200

    case 00

--- run_path_parallel.py
+++ run_path_parallel.py
@@ -231,8 +231,12 @@ def run_multiple_pathways(
 if __name__ == "__main__":
 
     PATHWAYS = [
-        "base_case_10_dd",
-        "high_feedstock_availability_10_dd"
+        "base_case_90_5_dd",
+        "base_case_80_5_dd",
+        "base_case_70_5_dd",
+        "base_case_60_5_dd",
+        "base_case_50_5_dd",
+        "base_case_00_5_dd"
     ]
 
     YEARS = ["2020", "2025", "2030", "2040", "2050"]
